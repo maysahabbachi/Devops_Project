6b2edcf7e192aef8f8cd1cf82ce73d8a4bd77318
Mohamed Neji Ghazouani <49163484+karunaro@users.noreply.github.com>
2021-11-12 00:54:12 +0100

    sonar fix

--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -13,7 +13,7 @@ public interface IEntrepriseService {
 	public int ajouterDepartement(Departement dep);
 	void affecterDepartementAEntreprise(int depId, int entrepriseId);
 	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
-	public void deleteEntrepriseById(int entrepriseId);
+	public int deleteEntrepriseById(int entrepriseId);
 	public void deleteDepartementById(int depId);
 	public Entreprise getEntrepriseById(int entrepriseId);
 }
