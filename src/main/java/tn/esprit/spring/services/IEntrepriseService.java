1f53f143730f43c087706a0dc3af57160003770a
maysahabbachi <maysa.habbachi@esprit.tn>
2021-11-11 14:26:15 +0100

    uodate

--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.services;
 
+import java.util.List;
 
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
@@ -10,6 +11,8 @@ public interface IEntrepriseService {
 	
 	public Integer ajouterEntreprise(EntrepriseDTo entreprise);
 	public int ajouterDepartement(Departement dep);
+	void affecterDepartementAEntreprise(int depId, int entrepriseId);
+	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
 	public void deleteEntrepriseById(int entrepriseId);
 	public void deleteDepartementById(int depId);
 	public Entreprise getEntrepriseById(int entrepriseId);
